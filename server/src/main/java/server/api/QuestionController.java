8a6a2704692785d8c92d148bb1d0752760e8d2fb
Attila Birke <A.B.Birke@student.tudelft.nl>
2022-02-25 15:16:32 +0100

    Added a PUSH and DELETE as well as a GET by ID request.

--- server/src/main/java/server/api/QuestionController.java
+++ server/src/main/java/server/api/QuestionController.java
@@ -3,12 +3,16 @@ package server.api;
 import commons.Quote;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.database.QuestionRepository;
 
 import java.util.List;
+import java.util.MissingResourceException;
+import java.util.NoSuchElementException;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/question")
 public class QuestionController {
@@ -21,10 +25,27 @@ public class QuestionController {
         return (List<Question>) repository.findAll();
     }
 
-    /*@GetMapping("{id}")
+    @GetMapping("{id}")
     public ResponseEntity<Question> getById(@PathVariable int id) {
         Question question = repository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Question doesn't exist with id: "+ id));
+                .orElseThrow(() -> new NoSuchElementException("Question doesn't exist with id: "+ id));
         return ResponseEntity.ok(question);
-    }*/
+    }
+
+    @PostMapping
+    public Question createQuestion(@RequestBody Question question){
+        return repository.save(question);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteQuestion(@PathVariable int id){
+        Question question = repository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Question not found with id: " + id));
+
+        repository.delete(question);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
 }
