15b4b6ff6efbf532b7cf4b1532af86c719b48c5a
Attila Birke <A.B.Birke@student.tudelft.nl>
2022-03-09 11:19:22 +0100

    Again unused imports.

--- client/src/main/java/client/scenes/SinglePlayer.java
+++ client/src/main/java/client/scenes/SinglePlayer.java
@@ -23,7 +23,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
-import java.util.Timer;
 
 import static com.google.inject.Guice.createInjector;
 
