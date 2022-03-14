8bd30f5d56d6933020021fc80015908e08823182
Octavian-Teodor Dragon <o.dragon@student.tudelft.nl>
2022-03-14 21:31:05 +0100

    Remove unused import
    At server/ServerUtils

--- server/src/main/java/server/ServerUtils.java
+++ server/src/main/java/server/ServerUtils.java
@@ -7,7 +7,6 @@ import server.database.ActivityRepository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.LinkedList;
-import java.util.Random;
 
 public class ServerUtils {
     /**
