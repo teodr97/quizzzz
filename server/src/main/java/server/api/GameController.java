00279d243fbb596d97eb006dbb8a6fda68c13321
Jordano <j.b.decastro@student.tudelft.nl>
2022-03-24 23:25:28 +0100

    checkstyleee

--- server/src/main/java/server/api/GameController.java
+++ server/src/main/java/server/api/GameController.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
