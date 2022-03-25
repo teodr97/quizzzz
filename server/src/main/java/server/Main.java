3601897d6348b08dfaee094eeeb6f6f817988714
Jordano <j.b.decastro@student.tudelft.nl>
2022-03-25 23:40:58 +0100

    Last checkstyle i promise

--- server/src/main/java/server/Main.java
+++ server/src/main/java/server/Main.java
@@ -18,7 +18,7 @@ package server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import server.Networking.ServerStream;
+
 
 
 @SpringBootApplication
